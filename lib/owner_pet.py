dc539b78e17934eba62936c58d8e16e9dcad6b8a
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-08-04 15:49:19 -0700

    done

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,37 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+    def get_type(self):
+        return self._pet_type
+
+    def set_type(self, pet_type):
+        if pet_type in self.PET_TYPES:
+            self._pet_type = pet_type
+        else:
+            raise Exception
+
+    pet_type = property(get_type, set_type)
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        sorted_list = sorted(self.pets(), key=lambda pet: pet.name)
+        return sorted_list
